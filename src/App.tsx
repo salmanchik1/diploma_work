8948b4e3558f5f2efb41e24c20966316c4949870
salmanchik1 <ramboy@mail.ru>
2025-01-17 01:04:05 +0300

    initial commit. Empty vite with ts + react. installed all necessary libraries

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import styled from "styled-components";
+
+const Title = styled.h1`
+    color: red;
+`;
+
+function App() {
+    return <Title>Hello</Title>;
+}
+
+export default App;
